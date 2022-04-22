f95fbd6d9b2bec3df1e839c1010ffc42615c4890
Mirshal Arief <101452195+Mirshal@users.noreply.github.com>
2022-04-22 22:21:45 +0700

    Delete koneksi.php

--- koneksi.php
+++
@@ -1,9 +0,0 @@
-<<<<<<< HEAD
-<?php 
-	$conn= new mysqli('localhost','root','','edel');
-=======
-<!-- membuat koneksi sistem antara lokal host dan database -->
-<?php 
-	$conn= new mysqli('localhost','root','','database');
->>>>>>> Mufadhdhal
-?>
