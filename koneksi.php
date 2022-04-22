d935e0545dcecd29d1b1d005aed5fe411573652d
Mirshal <mirshal20@mhs.unsyiah.ac.id>
2022-04-22 17:56:25 +0700

    Database

--- koneksi.php
+++ koneksi.php
@@ -1,3 +1,9 @@
+<<<<<<< HEAD
 <?php 
 	$conn= new mysqli('localhost','root','','edel');
+=======
+<!-- membuat koneksi sistem antara lokal host dan database -->
+<?php 
+	$conn= new mysqli('localhost','root','','database');
+>>>>>>> Mufadhdhal
 ?>
