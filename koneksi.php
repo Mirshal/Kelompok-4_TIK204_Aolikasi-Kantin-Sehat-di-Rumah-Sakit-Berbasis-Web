61ddbe89675bb69f0ff3cde967bf08805da483e2
Ahmad Mufadhdhal Alkifani <101258971+alkifani@users.noreply.github.com>
2022-04-23 00:30:18 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,7 +1,6 @@
 <!--<?php 
 	$conn= new mysqli('localhost','root','','edel');
-?>
--->
+?> -->
 <!-- membuat koneksi sistem antara lokal host dan database -->
 <?php 
 	$conn= new mysqli('localhost','root','','database');
