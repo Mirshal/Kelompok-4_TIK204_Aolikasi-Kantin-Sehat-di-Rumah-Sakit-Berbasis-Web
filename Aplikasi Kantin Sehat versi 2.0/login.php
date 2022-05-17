ff455855e3d84d42545a58bd7a5d5df4e5f7b1f5
Muhammad Dandy Pratama <101490528+dandypratamaa@users.noreply.github.com>
2022-05-17 11:50:54 +0700

    login

--- Aplikasi Kantin Sehat versi 2.0/login.php
+++ Aplikasi Kantin Sehat versi 2.0/login.php
@@ -4,7 +4,7 @@ include 'koneksi.php';
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>Login E-DEL</title>
+	<title>Login E-CANHEAL</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 <!--===============================================================================================-->
