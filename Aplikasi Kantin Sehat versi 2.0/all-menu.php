142eff64d2a19f5f16538f024bb809feb3a82b28
Ema Sari <101518159+emasari16@users.noreply.github.com>
2022-05-17 13:31:37 +0700

    all-menu

--- Aplikasi Kantin Sehat versi 2.0/all-menu.php
+++ Aplikasi Kantin Sehat versi 2.0/all-menu.php
@@ -270,7 +270,7 @@
  <div id="copyright">
     <div class="container">
         <div class="col-md-6">
-            <p class="pull-left">© E-DEL 2018</p>
+            <p class="pull-left">© E-CANHEAL 2022</p>
         </div>
         <div class="col-md-6">
             <p class="pull-right">Alright Reserved by 11Fingers
