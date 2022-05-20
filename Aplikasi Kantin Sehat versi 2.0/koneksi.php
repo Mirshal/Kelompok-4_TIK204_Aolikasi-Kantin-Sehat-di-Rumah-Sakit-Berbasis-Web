c9c6a1a77c4491806c7705a617ef0490fbf8d59b
Muhammad Dandy Pratama <101490528+dandypratamaa@users.noreply.github.com>
2022-05-20 15:06:31 +0700

    koneksi

--- Aplikasi Kantin Sehat versi 2.0/koneksi.php
+++ Aplikasi Kantin Sehat versi 2.0/koneksi.php
@@ -2,7 +2,7 @@
 <?php 
 	$conn= new mysqli('localhost','root','','edel');
 =======
-<!--  -->
+<!-- membuat koneksi sistem antara lokal host dengan database -->
 <?php 
 	$conn= new mysqli('localhost','root','','database');
 >>>>>>> Mufadhdhal
