1cb0816fc0e5b78241dccf002d9bdaf65b6605e6
Ahmad Mufadhdhal Alkifani <ahmad_m20@mhs.unsyiah.ac.id>
2022-05-21 10:48:38 +0700

    update

--- Aplikasi Kantin Sehat versi 2.0/koneksi.php
+++ Aplikasi Kantin Sehat versi 2.0/koneksi.php
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
-<?php 
-	$conn= new mysqli('localhost','root','','edel');
-=======
-<!-- membuat koneksi sistem antara lokal host dengan database -->
+<!-- membuat koneksi sistem antara lokal host dan database -->
 <?php 
 	$conn= new mysqli('localhost','root','','database');
->>>>>>> Mufadhdhal
 ?>
